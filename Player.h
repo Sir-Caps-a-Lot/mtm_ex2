3e32ddcb5d82dbc022aef23672bdeb0c24344b3d
ilay menahem <ilay.menahem@students.technion.ac.il>
2022-12-09 20:06:15 +0200

    added documentation to player.h

--- Player.h
+++ Player.h
@@ -10,23 +10,92 @@
 
 class Player {
 public:
+	/*
+     * C'tor of Player class
+     *
+     * @param name - The type of the Player (optional when useing the default counstractor).
+     * @param max hp - of the player (optional if power is inisilised)
+	 * @param power - of the player if (optional)
+     * @return new instance of Player.
+    */
 	Player(const char* name);
 	Player(const char* name, int max_hp);
 	Player(const char* name, int max_hp, int power);
 	Player();
+
+	/**
+	 * default D'tor
+	*/
+	~Player() = default;
+
+	/**
+	 * 
+	*/
 	void operator=(const Player& player);
 	
+	/**
+	 * prints the parameters of player in the following format
+	 * Player Details:
+	 * Name: 
+	 * Gandalf Level: 1
+	 * Force: 5
+	 * HP: 300
+	 * Coins: 0
+	*/
 	void printInfo() const;
+
+	/**
+	 * increases the level of player by one
+	 * if his level is lower form 10 otherwise i'll do nothing
+	*/
 	void levelUp();
+
+	/**
+	 * @return the level of player
+	*/
 	int getLevel() const;
+
+	/**
+	 * @return the Attack Strength of player
+	*/
 	int getAttackStrength() const;
+
+	/**
+	 * riases the player level by power_up
+	 * @param power_up - how much to riase the player level
+	*/
 	void buff(int power_up);
+
+	/**
+	 * raises the health of player by add_hp or to ten whichever is lower
+	 * @param add_hp how much hp to add
+	*/
 	void heal(int add_hp);
+
+	/**
+	 * lowers the health of player by damage if it's lower then 0 it does nothing
+	 * @param damage by how much to lower hp
+	*/
 	void damage(int damage);
+
+	/**
+	 * gives player the amount of coins
+	 * @param coins the amount of coins to add
+	*/
 	void addCoins(int coins);
+
+	/**
+	 * checks if the player's health is 0 or lower
+	 * @return if the player is knocked out
+	*/
 	bool isKnockedOut() const;
+
+	/**
+	 * lowers the player's coins by cost if he can pay otherwise he dosen't pay anything
+	 * @param cost how much player has to pay
+	 * @return if the player made the payment
+	*/
 	bool pay(int cost);
-	~Player() = default;
 
 private:
 	const char* m_name;
